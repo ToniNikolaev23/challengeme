e3c2f95f950b9870f1df21cf1a3264b509ed174f
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-03-02 23:04:37 +0200

    Install supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://zfprnrawdheefxonvzpk.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmcHJucmF3ZGhlZWZ4b252enBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NDg0ODAsImV4cCI6MjA1NjUyNDQ4MH0.q6i40UKWM-GzU2GlUvtOWDX8PrawvDPbWr8y7GY2uDk";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
